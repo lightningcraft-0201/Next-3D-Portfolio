853b6d35602a6b50ac49a4adb3275aaa2a239d79
DevRex-0201 <dev.rex.0201@gmail.com>
2023-11-10 14:05:45 +0900

    uploaded 3d portfolio

---
+++ src/js/UI/About/skills.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        name: 'AutoCad',
+        width: '90%',
+    },
+    {
+        name: 'Revit',
+        width: '80%',
+    },
+    {
+        name: 'Sketch Up',
+        width: '100%',
+    },
+    {
+        name: '3DS Max',
+        width: '80%'
+    },
+    {
+        name: 'Blender',
+        width: '70%',
+    },
+    {
+        name: 'Unity 3D',
+        width: '90%',
+    },
+    {
+        name: 'V-ray',
+        width: '100%',
+    },
+    {
+        name: 'Photoshop',
+        width: '100%',
+    }
+]
---
+++ src/js/UI/Work/items.js
@@ -0,0 +1,214 @@
+export default [
+  {
+    id: 0,
+    name: "",
+    description: "Exterior Design 1",
+    image: "images/projects/img_1.jpg",
+    tags: ["AutoCad", "SketchUp", "Vray"],
+    liveview: "https://osso-heco.webflow.io/",
+    alt: "Hospital website portfolio project",
+  },
+  {
+    id: 1,
+    name: "",
+    description: "Exterior Design2",
+    image: "images/projects/img_2.jpg",
+    tags: ["AutoCad", "SketchUp", "Vray", "ThreeDSMax"],
+    liveview: "https://cryptomines.app/",
+    alt: "Mint NFTs, Explore Planets portfolio project",
+  },
+  {
+    id: 2,
+    name: "",
+    description: "Exterior Design 3",
+    image: "images/projects/img_3.png",
+    tags: ["AutoCad", "SketchUp", "ThreeDSMax"],
+    liveview: "https://glovoapp.com/",
+    alt: "Food Delivery portfolio project",
+  },
+  {
+    id: 4,
+    name: "",
+    description:
+      "Interior Design",
+    liveview: "https://www.drumelia.com/",
+    image: "images/projects/img_5.png",
+    tags: ["AutoCad", "SketchUp", "Vray", "DialUx", "Photoshop"],
+    alt: "This is the Luxury Real Estate website",
+  },
+  {
+    id: 5,
+    name: "",
+    description:
+      "Interior Design",
+    liveview: "https://www.dexify.finance/",
+    image: "images/projects/img_6.jpg",
+    tags: ["AutoCad", "ThreeDSMax", "Photoshop", "Vray", "DialUx"],
+    alt: "Dexify Finance",
+  },
+  {
+    id: 6,
+    name: "",
+    description:
+      "Interior Design",
+    image: "images/projects/img_10.png",
+    liveview: "https://nftshowroom.com/",
+    tags: ["AutodeskRevit", "ThreeDSMax", "Photoshop", "Vray", "DialUx"],
+    alt: "",
+  },
+  {
+    id: 6,
+    name: "",
+    description:
+      "Bathroom Design",
+    image: "images/projects/img_7.png",
+    liveview: "https://nftshowroom.com/",
+    tags: ["AutoCad", "ThreeDSMax", "Photoshop", "DialUx"],
+    alt: "",
+  },
+  {
+    id: 6,
+    name: "",
+    description:
+      "Bathroom Design",
+    image: "images/projects/img_8.jpg",
+    liveview: "https://nftshowroom.com/",
+    tags: ["AutoCad", "ThreeDSMax", "Photoshop", "DialUx"],
+    alt: "",
+  },
+  {
+    id: 6,
+    name: "",
+    description:
+      "Bathroom Design",
+    image: "images/projects/img_9.jpg",
+    liveview: "https://nftshowroom.com/",
+    tags: ["AutoCad", "ThreeDSMax", "Photoshop", "DialUx"],
+    alt: "",
+  },
+  {
+    id: 6,
+    name: "",
+    description:
+      "2D Floor Plan",
+    image: "images/projects/img_12.jpg",
+    liveview: "https://nftshowroom.com/",
+    tags: ["AutoCad", "SketchUp"],
+    alt: "",
+  },
+  {
+    id: 6,
+    name: "",
+    description:
+    "2D Floor Plan",
+    image: "images/projects/img_13.webp",
+    liveview: "https://nftshowroom.com/",
+    tags: ["AutoCad", "SketchUp"],
+    alt: "",
+  },
+  {
+    id: 6,
+    name: "",
+    description:
+    "2D Floor Plan",
+    image: "images/projects/img_14.jpg",
+    liveview: "https://nftshowroom.com/",
+    tags: ["AutoCad", "SketchUp"],
+    alt: "",
+  },
+  {
+    id: 6,
+    name: "",
+    description:
+    "2D Floor Plan",
+    image: "images/projects/img_151.jpg",
+    liveview: "https://nftshowroom.com/",
+    tags: ["AutoCad", "SketchUp"],
+    alt: "",
+  },
+  {
+    id: 6,
+    name: "",
+    description:
+      "3D Floor Plan",
+    image: "images/projects/img_15.png",
+    liveview: "https://nftshowroom.com/",
+    tags: ["AutoCad", "AutodeskRevit", "SketchUp"],
+    alt: "",
+  },
+  {
+    id: 6,
+    name: "",
+    description:
+    "3D Floor Plan",
+    image: "images/projects/img_16.png",
+    liveview: "https://nftshowroom.com/",
+    tags: ["AutoCad", "AutodeskRevit", "SketchUp"],
+    alt: "",
+  },
+  {
+    id: 6,
+    name: "",
+    description:
+      "3D Game Modelling",
+    image: "images/projects/img_17.jpg",
+    liveview: "https://nftshowroom.com/",
+    tags: ["Blender", "ThreeDSMax", "Photoshop"],
+    alt: "",
+  },
+  {
+    id: 6,
+    name: "",
+    description:
+    "3D Game Modelling",
+    image: "images/projects/img_18.jpg",
+    liveview: "https://nftshowroom.com/",
+    tags: ["Blender", "ThreeDSMax", "Photoshop"],
+    alt: "",
+  },
+  {
+    id: 6,
+    name: "",
+    description:
+    "3D Game Modelling",
+    image: "images/projects/img_19.jpg",
+    liveview: "https://nftshowroom.com/",
+    tags: ["Blender", "ThreeDSMax", "Photoshop"],
+    alt: "",
+  },
+  {
+    id: 6,
+    name: "",
+    description:
+      "Mow Mission Design",
+    image: "images/projects/img_20.png",
+    liveview: "https://nftshowroom.com/",
+    tags: ["GemEditor"],
+    alt: "",
+  },
+  {
+    id: 6,
+    name: "",
+    description:
+    "Mow Mission Design",
+    image: "images/projects/img_21.png",
+    liveview: "https://nftshowroom.com/",
+    tags: ["GemEditor"],
+    alt: "",
+  },
+  
+  {
+    id: 6,
+    name: "",
+    description:
+    "Mow Mission Design",
+    image: "images/projects/img_22.png",
+    liveview: "https://nftshowroom.com/",
+    tags: ["GemEditor"],
+    alt: "",
+  },
+  
+  
+  
+  
+];
